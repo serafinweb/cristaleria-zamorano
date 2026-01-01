69c826ecefb6d3bd7d46e5ba237954f206ba2514
serafinweb <noticiaserafin@gmail.com>
2026-01-01 21:35:05 +0100

    fix: header component completo y funcional

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,44 +1,68 @@
-<header className="header">
+import React, { useState, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo-cz.png';
+import logoTipografico from '../assets/logo-tipografico.PNG';
+import './Header.css';
 
-  <div className="header-container">
+const Header = () => {
+  const [open, setOpen] = useState(false);
+  const menuRef = useRef(null);
 
-    {/* IZQUIERDA: logo + logotipo */}
-    <div className="header-left">
+  const handleMouseEnter = () => setOpen(true);
 
-      <div
-        className="logo"
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-        onClick={() => setOpen(!open)}
-      >
-        <img src={logo} alt="Cristalería Zamorano" />
+  const handleMouseLeave = (e) => {
+    if (menuRef.current && !menuRef.current.contains(e.relatedTarget)) {
+      setOpen(false);
+    }
+  };
+
+  return (
+    <header className="header">
+
+      {/* CONTENEDOR PRINCIPAL */}
+      <div className="header-container">
+
+        {/* IZQUIERDA: logo + logotipo */}
+        <div className="header-left">
+
+          <div
+            className="logo"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            onClick={() => setOpen(!open)}
+          >
+            <img src={logo} alt="Cristalería Zamorano" />
+          </div>
+
+          <div className="logo-text">
+            <img src={logoTipografico} alt="Cristalería Zamorano" />
+          </div>
+
+        </div>
+
+        {/* DERECHA: teléfono */}
+        <div className="header-right">
+          <a href="tel:+34955631356" className="phone-btn">
+            955 631 356
+          </a>
+        </div>
+
       </div>
 
-      <div className="logo-text">
-        <img src={logoTipografico} alt="Cristalería Zamorano" />
-      </div>
+      {/* MENÚ FLOTANTE */}
+      {open && (
+        <div
+          className="menu-flotante"
+          ref={menuRef}
+          onMouseLeave={() => setOpen(false)}
+        >
+          <Link to="/">Inicio</Link>
+          <Link to="/servicios">Servicios</Link>
+        </div>
+      )}
 
-    </div>
+    </header>
+  );
+};
 
-    {/* DERECHA: teléfono */}
-    <div className="header-right">
-      <a href="tel:+34955631356" className="phone-btn">
-        955 631 356
-      </a>
-    </div>
-
-  </div>
-
-  {/* MENÚ FLOTANTE */}
-  {open && (
-    <div
-      className="menu-flotante"
-      ref={menuRef}
-      onMouseLeave={() => setOpen(false)}
-    >
-      <Link to="/">Inicio</Link>
-      <Link to="/servicios">Servicios</Link>
-    </div>
-  )}
-
-</header>
+export default Header;
